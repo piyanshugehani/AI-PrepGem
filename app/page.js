5484ec990c89e26c57f2550d5a353e438bd5d6b5
piyanshugehani <piyanshugehani@gmail.com>
2024-11-14 00:59:59 +0530

    url signin

--- app/page.js
+++ app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
   }, []);
 
   const handleStartClick = () => {
-    router.push('/signin'); // Redirect to /signin page
+    router.push('/sign-in'); // Redirect to /signin page
   };
 
   return (
